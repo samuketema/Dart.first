12b51d29b895a18cd6b8888a1f0bba0b66abac1e
Samuel <samuelketema01@gmail.com>
2024-02-29 12:49:58 +0300

    charachter package is added

--- bin/dart_application.dart
+++ bin/dart_application.dart
@@ -1,4 +1,5 @@
 import 'package:dart_application/dart_application.dart' as dart_application;
+import 'package:characters/characters.dart';
 // this is my first code
 void main(List<String> arguments) {
   print('Hello world: ${dart_application.calculate()}!');
